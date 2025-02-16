e3ee29b6120225553e98524fafc5b02b2b474fc9
Brier Hehmeyer <bmhehmeyer@gmail.com>
2025-02-15 17:57:30 -0800

    Finished back propagation to work for the last layer. Added a way to save and load the model. I also added biases after, but they did not really improve the performance of the model, maybe I have to take a closer look at them later. Also trained two models, one with biases and one without. The accuracy for both is about 94%, with the one with biases being a little higher.

--- First Number Model/main.py
+++ First Number Model/main.py
@@ -24,7 +24,7 @@ def relu(x):
 
 
 def relu_derivative(x):
-    return x > 0
+    return np.maximum(x, 0)
 
 
 def softmax(x):
@@ -53,12 +53,19 @@ def softmax_derivative(x):
 
 
 def forward_pass(input_data, input_to_hidden_weights, hidden_to_output_weights):
+    # For bias in input layer
+    input_data = np.append(input_data, 1)
+
     z_data = []
     a_data = []
 
     output = np.dot(input_data, input_to_hidden_weights)
     z_data.append(output)
     output = relu(output)
+
+    # For bias in hidden layer
+    output = np.append(output, 1)
+
     a_data.append(output)
 
     output = np.dot(output, hidden_to_output_weights)
@@ -69,28 +76,48 @@ def forward_pass(input_data, input_to_hidden_weights, hidden_to_output_weights):
     return z_data, a_data
 
 
-def backwards_pass(z_data, a_data, label):
-    gradient = []
+def backwards_pass(z_data, a_data, label, hidden_to_output_weights, input_data):
+    # For bias in input layer
+    input_data = np.append(input_data, 1)
 
     # Gradient of cost function with respect to model output neurons.
     dc_da2 = -(1 / 5) * (label - a_data[1])
 
     softmax_derivative_jacobian = softmax_derivative(z_data[1])
 
+    # Gradient of cost function with respect to output neurons before activation function (softmax)
+    dc_dz2 = np.dot(dc_da2, softmax_derivative_jacobian)
+
     # Computed by multiplying dc_dz2 and dz2_dw
-    last_layer_gradient = np.dot(dc_da2, softmax_derivative_jacobian) * a_data[0][:, np.newaxis]
-    return last_layer_gradient
+    last_layer_gradient = dc_dz2 * a_data[0][:, np.newaxis]
+
+    # dz2_da1 (which is just the hidden_to_output_weights) * da1_dz1 (the derivative of the activation function, relu)
+    # Need to exclude last weight, as that is for bias
+    dz2_dz1 = hidden_to_output_weights[:-1, :] * relu_derivative(z_data[0])[:, np.newaxis]
+    dc_dz1 = np.dot(dc_dz2, dz2_dz1.T)
+
+    # The input_data, or z_0, is dz1_dw
+    first_layer_gradient = dc_dz1 * input_data[:, np.newaxis]
+
+    return first_layer_gradient, last_layer_gradient
 
 
 def main():
-    input_num = 784
-    hidden_neuron_num = 10
+    # Plus 1 for the biases
+    input_num = 784 + 1
+    hidden_neuron_num = 16 + 1
     output_num = 10
 
-    learning_rate = 0.03
+    learning_rate = 0.01
 
-    input_to_hidden_weights = np.random.rand(input_num, hidden_neuron_num) - 0.5
+    save_as = "model_weights_bias"
+
+    # -1 in input to hidden_weights so nothing is connected to the bias neuron.
+    input_to_hidden_weights = np.random.rand(input_num, hidden_neuron_num - 1) - 0.5
     hidden_to_output_weights = np.random.rand(hidden_neuron_num, output_num) - 0.5
+    # weights = np.load("model_weights.npz")
+    # input_to_hidden_weights = weights["input_to_hidden"]
+    # hidden_to_output_weights = weights["hidden_to_output"]
 
     train_images, train_labels, test_images, test_labels = load_data()
     possible_labels = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
@@ -118,14 +145,19 @@ def main():
     print()
 
     print("Starting training...")
-    for i in range(10):
+    for i in range(30):
         total_loss = 0
+        total_correct = 0
         for j in range(len(train_images)):
             z_data, a_data = forward_pass(train_images[j], input_to_hidden_weights, hidden_to_output_weights)
+            if np.argmax(a_data[1]) == np.argmax(ohe_train_labels[j]):
+                total_correct += 1
             total_loss += loss(a_data[1], ohe_train_labels[j])
-            gradient = backwards_pass(z_data, a_data, ohe_train_labels[j])
-            hidden_to_output_weights -= gradient * learning_rate
-        print(f"Finished epoch {i + 1} with an average loss of {total_loss / len(train_images)}")
+            first_layer_gradient, last_layer_gradient = backwards_pass(z_data, a_data, ohe_train_labels[j], hidden_to_output_weights, train_images[j])
+            input_to_hidden_weights -= first_layer_gradient * learning_rate
+            hidden_to_output_weights -= last_layer_gradient * learning_rate
+        print(f"Finished epoch {i + 1} with an average loss of {total_loss / len(train_images)} and num correct {total_correct}/{len(train_images)}")
+    print()
 
     print("Testing final model...")
     total_loss = 0
@@ -138,6 +170,9 @@ def main():
             num_correct += 1
 
     print(f"final model has loss of {total_loss / len(test_images)} with the number of correct guesses being {num_correct}/{len(test_images)}")
+    print("Saving model...")
+    np.savez(save_as + ".npz", input_to_hidden=input_to_hidden_weights, hidden_to_output=hidden_to_output_weights)
+    print(f"Saved model as {save_as}.npz")
 
 
 if __name__ == "__main__":
